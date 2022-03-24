f8980aabfd5ef4ea768b759d20e23504d6c7aa81
deepthikotla <deepthidinesh@LAPTOP_Q7BVHQ8M>
2022-03-24 18:52:19 +0530

    assignment 2

---
+++ remove.py
@@ -0,0 +1,3 @@
+#python- remove empty list from list
+lst =[[1,2,3],[],[3,4],[],[4,5,6]]
+print([x for x in lst if x])
---
+++ sort.py
@@ -0,0 +1,4 @@
+#python program to sort the list of values
+numbers = [1,3,4,8, -9]
+numbers.sort()
+print(numbers)
